1f774b247469230216fecf92d1b93e7dfbebd192
yangyong <yangyong@pxsj.com>
2017-01-04 16:58:07 +0800

    commit 1.31

--- calc.py
+++ calc.py
@@ -3,12 +3,33 @@ import math
 def C(n,r):
     return math.factorial(n) / (math.factorial(n-r) * math.factorial(r))
 
+
+# 1.30
 sum = 0
 for i in range(0,7):    
-    sum += C(6,i) * math.factorial(i + 1) * math.factorial(7-i)
+    sum += C(6,i) * math.factorial(i + 1) * math.factorial(7-i) 
 
 for i in range(1,7):
     sum += C(6,i) * math.factorial(i) * math.factorial(8-i)
 
 print( 4 * sum)
-    
+
+# 1.31
+
+def h(n,r):
+    if (r == 1):
+        return 1
+    elif(r == 2):
+        return C(n-1,1)
+    else:
+        s = 0
+        for i in range(1,n-1):
+            s += h(n-i,r-1)
+        return s
+
+print(h(8,4))
+sum = 0 
+for i in range(1,5):
+    sum += C(4,i) * h(8,i)
+
+print(sum)
